77637676a622e1f2d746ee9e66f1597d34617a79
Simone Celia <simone.celia@simonecelia.it>
2024-10-01 01:52:35 +0200

    slot_esposti

--- src/main/java/it/simonecelia/edentbbuilder/service/SlotService.java
+++ src/main/java/it/simonecelia/edentbbuilder/service/SlotService.java
@@ -3,11 +3,17 @@ package it.simonecelia.edentbbuilder.service;
 import it.simonecelia.edentbbuilder.entity.Slot;
 import jakarta.enterprise.context.ApplicationScoped;
 
+import java.util.List;
+
 
 @ApplicationScoped
 public class SlotService {
 
 	public Slot getByName ( String name ) {
 		return Slot.find ( "LOWER(slot) = LOWER(?1)", name ).firstResult ();
+	}
+
+	public List<Slot> findAllSlots () {
+		return Slot.listAll ();
 	}
 }
