ad95f50cb4c2db415a61f28c0a89c6a60aa9a98d
Simone Celia <simone.celia@simonecelia.it>
2024-09-30 02:44:21 +0200

    todo_dto

---
+++ src/main/java/it/simonecelia/edentbbuilder/dto/ItemDTO.java
@@ -0,0 +1,13 @@
+package it.simonecelia.edentbbuilder.dto;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+
+public class ItemDTO implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = -7320256725929878523L;
+
+
+}
---
+++ src/main/java/it/simonecelia/edentbbuilder/service/ItemService.java
@@ -0,0 +1,14 @@
+package it.simonecelia.edentbbuilder.service;
+
+import it.simonecelia.edentbbuilder.dto.ItemDTO;
+import it.simonecelia.edentbbuilder.entity.Item;
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class ItemService {
+
+	public Item create ( ItemDTO itemDTO ) {
+		return null; //TODO
+	}
+}
