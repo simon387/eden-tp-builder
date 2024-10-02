4a7ed4cb49f7e8d0fb8faa8f7cb77ea933592b47
Simone Celia <simone.celia@simonecelia.it>
2024-10-02 13:24:56 +0200

    new_char_controller

--- src/main/java/it/simonecelia/edentbbuilder/controller/CharacterController.java
+++ src/main/java/it/simonecelia/edentbbuilder/controller/CharacterController.java
@@ -1,5 +1,26 @@
 package it.simonecelia.edentbbuilder.controller;
 
+import io.quarkus.logging.Log;
+import it.simonecelia.edentbbuilder.service.CharacterService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+
+@Path ( "/character" )
 public class CharacterController {
 
+	@Inject
+	CharacterService characterService;
+
+	@GET
+	@Produces ( MediaType.APPLICATION_JSON )
+	public List<String> getAllCharacters () { //TODO
+		Log.info ( "Calling getAllCharacters" );
+		return null;
+	}
 }
---
+++ src/main/java/it/simonecelia/edentbbuilder/service/CharacterService.java
@@ -0,0 +1,9 @@
+package it.simonecelia.edentbbuilder.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+@ApplicationScoped
+public class CharacterService {
+
+}
