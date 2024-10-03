7c3dd5368e8fbb777c7aa4a3e17233f2b9e96ce8
Simone Celia <simone.celia@eng.it>
2024-10-03 21:42:39 +0200

    scheletro_sout

--- src/main/java/it/simonecelia/edentbbuilder/service/ReportService.java
+++ src/main/java/it/simonecelia/edentbbuilder/service/ReportService.java
@@ -38,7 +38,7 @@ public class ReportService {
 		short charismaCap = 0;
 		short empathy = 0;
 		short empathyCap = 0;
-		short hits;
+		short hits = 0;
 		short hitsCap = 0;
 		short power = 0; // senza cap a 25 ?
 		short powerCap = 0;
@@ -60,14 +60,48 @@ public class ReportService {
 		short debuffEffectiveness = 0;
 		short buffEffectiveness = 0;
 		short healingEffectiveness = 0;
-		short spellDuration;
+		short spellDuration = 0;
 		short castingSpeed = 0;
 		short spellRange = 0;
 		short spellDamage = 0;
 		short resistPierce = 0;
 
-		System.out.println ( "Statistic" );
+		System.out.println ( "\nStatistic" );
+		System.out.println ( "Strength: " + strength + "/" + ( 75 + strengthCap ) );
+		System.out.println ( "Constitution: " + constitution + "/" + ( 75 + constitutionCap ) );
+		System.out.println ( "Dexterity: " + dexterity + "/" + ( 75 + dexterityCap ) );
+		System.out.println ( "Quickness: " + quickness + "/" + ( 75 + quicknessCap ) );
+		System.out.println ( "Intelligence: " + intelligence + "/" + ( 75 + intelligenceCap ) );
+		System.out.println ( "Piety: " + piety + "/" + ( 75 + pietyCap ) );
+		System.out.println ( "Charisma: " + charisma + "/" + ( 75 + charismaCap ) );
+		System.out.println ( "Empathy: " + empathy + "/" + ( 75 + empathyCap ) );
+		System.out.println ( "Hits: " + hits + "/" + ( 200 + hitsCap ) );
+		System.out.println ( "Power: " + power + "/" + ( 25 + powerCap ) );
 
+		System.out.println ( "\nResistance" );
+		System.out.println ( "Body: " + body + "/26" );
+		System.out.println ( "Cold: " + cold + "/26" );
+		System.out.println ( "Heat: " + heat + "/26" );
+		System.out.println ( "Energy: " + energy + "/26" );
+		System.out.println ( "Matter: " + matter + "/26" );
+		System.out.println ( "Spirit: " + spirit + "/26" );
+		System.out.println ( "Crush: " + crush + "/26" );
+		System.out.println ( "Thrust: " + thrust + "/26" );
+		System.out.println ( "Slash: " + slash + "/26" );
+
+		System.out.println ( "\nSkill" );
+		System.out.println ( "All Magic Skill: " + allMagicSkills + "/11" );
+
+		System.out.println ( "\nTOA Bonus" );
+		System.out.println ( "Armour Factor: " + armorFactor + "/" + ( 50 + armorFactorCap ) );
+		System.out.println ( "Debuff Effectiveness: " + debuffEffectiveness + "/25" );
+		System.out.println ( "Buff Effectiveness: " + buffEffectiveness + "/25" );
+		System.out.println ( "Healing Effectiveness: " + healingEffectiveness + "/25" );
+		System.out.println ( "Spell Duration: " + spellDuration + "/25" );
+		System.out.println ( "Casting Speed: " + castingSpeed + "/10" );
+		System.out.println ( "Spell Range: " + spellRange + "/10" );
+		System.out.println ( "Spell Damage: " + spellDamage + "/10" );
+		System.out.println ( "Resist Pierce: " + resistPierce + "/10" );
 
 		return report;
 	}
