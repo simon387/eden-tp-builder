34124ffced505fbf4974961c86689e131828f668
Simone Celia <simone.celia@simonecelia.it>
2024-10-03 20:33:45 +0200

    predisposizioni_reoprt

---
+++ src/main/java/it/simonecelia/edentbbuilder/model/Report.java
@@ -0,0 +1,13 @@
+package it.simonecelia.edentbbuilder.model;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+
+public class Report implements Serializable {
+
+	@Serial
+	private static final long serialVersionUID = 545883382056982722L;
+
+
+}
--- src/main/java/it/simonecelia/edentbbuilder/service/ReportService.java
+++ src/main/java/it/simonecelia/edentbbuilder/service/ReportService.java
@@ -1,13 +1,23 @@
 package it.simonecelia.edentbbuilder.service;
 
+import it.simonecelia.edentbbuilder.model.Report;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.core.Response;
+import jakarta.inject.Inject;
 
 
 @ApplicationScoped
 public class ReportService {
 
-	public Response generateByCharacterId ( Long id ) {
+	@Inject
+	CharacterService characterService;
+
+	public Report generateByCharacterId ( Long id ) {
+		var character = characterService.getCharaceterById ( id );
+		if ( character == null ) {
+			return null;
+		}
+
+
 		return null;
 	}
 }
