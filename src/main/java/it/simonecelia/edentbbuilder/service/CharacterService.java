3d210387ce793f82abc48ca70dd4f8bf3dee7a57
Simone Celia <simone.celia@eng.it>
2024-10-03 20:02:36 +0200

    file_per_salvataggio_pg

--- src/main/java/it/simonecelia/edentbbuilder/service/CharacterService.java
+++ src/main/java/it/simonecelia/edentbbuilder/service/CharacterService.java
@@ -3,6 +3,7 @@ package it.simonecelia.edentbbuilder.service;
 import it.simonecelia.edentbbuilder.dto.CharacterDTO;
 import it.simonecelia.edentbbuilder.entity.Character_;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 
 import java.util.List;
@@ -11,12 +12,45 @@ import java.util.List;
 @ApplicationScoped
 public class CharacterService {
 
+	@Inject
+	ClassService classService;
+
+	@Inject
+	ItemService itemService;
+
 	public List<Character_> getAllCharaceters () {
 		return Character_.listAll ();
 	}
 
 	@Transactional
 	public Character_ create ( CharacterDTO characterDTO ) {
-		return null;
+		var character = new Character_ ();
+		character.setName ( characterDTO.getName () );
+		character.setLevel ( characterDTO.getLevel () );
+		character.setClass_ ( classService.getByName ( characterDTO.getClassName () ) );
+
+		//TODO non c'è il check sugli slot, sticazzi
+		character.setArms ( itemService.getByName ( characterDTO.getArms () ) );
+		character.setChest ( itemService.getByName ( characterDTO.getChest () ) );
+		character.setCloak ( itemService.getByName ( characterDTO.getCloak () ) );
+		character.setDistance ( itemService.getByName ( characterDTO.getDistance () ) );
+		character.setBelt ( itemService.getByName ( characterDTO.getBelt () ) );
+		character.setFeet ( itemService.getByName ( characterDTO.getBoots () ) );
+		character.setHands ( itemService.getByName ( characterDTO.getGloves () ) );
+		character.setHelm ( itemService.getByName ( characterDTO.getHelm () ) );
+		character.setJewel ( itemService.getByName ( characterDTO.getJewel () ) );
+		character.setlBracer ( itemService.getByName ( characterDTO.getlBracer () ) );
+		character.setrBracer ( itemService.getByName ( characterDTO.getrBracer () ) );
+		character.setlRing ( itemService.getByName ( characterDTO.getlRing () ) );
+		character.setrRing ( itemService.getByName ( characterDTO.getrRing () ) );
+		character.setLegs ( itemService.getByName ( characterDTO.getLegs () ) );
+		character.setMythirian ( itemService.getByName ( characterDTO.getMythirian () ) );
+		character.setNecklace ( itemService.getByName ( characterDTO.getNecklace () ) );
+		character.setOffHand ( itemService.getByName ( characterDTO.getLeftHand () ) );
+		character.setOneHand ( itemService.getByName ( characterDTO.getRightHand () ) );
+		character.setTwoHand ( itemService.getByName ( characterDTO.getTwoHanded () ) );
+
+		character.persist ();
+		return character;
 	}
 }
