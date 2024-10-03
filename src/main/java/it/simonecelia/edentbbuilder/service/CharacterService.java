e52cb6605c8ae3e85e83f78d5231058b7aca9b7c
Simone Celia <simone.celia@eng.it>
2024-10-03 18:54:12 +0200

    get_all__chars_service

--- src/main/java/it/simonecelia/edentbbuilder/service/CharacterService.java
+++ src/main/java/it/simonecelia/edentbbuilder/service/CharacterService.java
@@ -1,9 +1,15 @@
 package it.simonecelia.edentbbuilder.service;
 
+import it.simonecelia.edentbbuilder.entity.Character_;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.List;
 
 
 @ApplicationScoped
 public class CharacterService {
 
+	public List<Character_> getAllCharaceters () {
+		return Character_.listAll ();
+	}
 }
