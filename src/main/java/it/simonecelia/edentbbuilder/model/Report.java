e06450aa451a5146beb3e704ae55076113649473
Simone Celia <simone.celia@simonecelia.it>
2024-10-03 21:05:15 +0200

    test_d_reportistica

--- src/main/java/it/simonecelia/edentbbuilder/model/Report.java
+++ src/main/java/it/simonecelia/edentbbuilder/model/Report.java
@@ -9,5 +9,15 @@ public class Report implements Serializable {
 	@Serial
 	private static final long serialVersionUID = 545883382056982722L;
 
+	private String title;
 
+	//TODO
+
+	public String getTitle () {
+		return title;
+	}
+
+	public void setTitle ( String title ) {
+		this.title = title;
+	}
 }
