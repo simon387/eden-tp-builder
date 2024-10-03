b79e4e313ae3e4536b157b7135e0e7159b847295
Simone Celia <simone.celia@eng.it>
2024-10-03 20:29:04 +0200

    aggiunte_classi_report

---
+++ src/main/java/it/simonecelia/edentbbuilder/controller/ReportController.java
@@ -0,0 +1,27 @@
+package it.simonecelia.edentbbuilder.controller;
+
+import io.quarkus.logging.Log;
+import it.simonecelia.edentbbuilder.service.ReportService;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+
+@Path ( "/report/character" )
+public class ReportController {
+
+	@Inject
+	ReportService reportService;
+
+	@GET
+	@Path ( "/{id}" )
+	@Produces ( MediaType.APPLICATION_JSON )
+	public Response generateReport ( @PathParam ( "id" ) Long id ) {
+		Log.info ( "Calling generateReport" );
+		return reportService.generateByCharacterId ( id );
+	}
+}
---
+++ src/main/java/it/simonecelia/edentbbuilder/service/ReportService.java
@@ -0,0 +1,13 @@
+package it.simonecelia.edentbbuilder.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.core.Response;
+
+
+@ApplicationScoped
+public class ReportService {
+
+	public Response generateByCharacterId ( Long id ) {
+		return null;
+	}
+}
